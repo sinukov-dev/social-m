ccd9f882d8e5ac2d7eb20442e570d2facec6006d
Maxim Sinyukov <sinukov.dev@gmail.com>
2024-08-31 00:51:47 +0200

    how fix the types in env.local

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
-			NEXT_PUBLIC_PROJECT_ID: string
+			NEXT_PUBLIC_APPWRITE_PROJECT: string
 			NEXT_PUBLIC_APPWRITE_ENDPOINT: string
 		}
 	}
